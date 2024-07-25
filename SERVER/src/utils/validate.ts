8987f7d7beaafcaacebfe573e46901df1c066292
phong phan <106137380+phongphanq089@users.noreply.github.com>
2024-07-25 15:31:55 +0700

    deploy to render v9

--- SERVER/src/utils/validate.ts
+++ SERVER/src/utils/validate.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import { ValidationChain, validationResult } from 'express-validator'
-// import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
+import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
 import HTTP_STATUS from '~/constants/httpStatus'
 import { EntityErorr, errorWithStatus } from '~/models/errors'
 
-export const validate = (validations: any) => {
+export const validate = (validations: RunnableValidationChains<ValidationChain>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     await validations.run(req)
     const errors = validationResult(req)
